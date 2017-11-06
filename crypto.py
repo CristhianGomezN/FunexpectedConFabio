80861f3edf63edc2f2cdaa37a893859643a4d006
miaortizma <miaortizma@unal.edu.co>
2017-11-05 21:33:31 -0500

    pruebas para texto plano

--- crypto.py
+++ crypto.py
@@ -20,8 +20,9 @@ class block:
         for i in range(n):
             for j in range(i+1,n):
                 U[i][j]=rnd.randint(0,1)
-                L[i][j]=rnd.randint(0,1)
-        M=U.dot(L.transpose())
+                L[j][i]=rnd.randint(0,1)        
+        M=U.dot(L)
+        np.linalg.inv(M%2)
         return M%2
     
     '''
@@ -29,15 +30,16 @@ class block:
     '''
     def generarfuncion(n, mat, arr, s, automaton):
         rule = []
+#        print(s, automaton.lPermitida ,len(automaton.lPermitida))
         for i in range(n):
             cur = []
             for j in range(n):
                 if(mat[i][j]):
                     add = [arr[j]]
                     if(s > 0 and rnd.randint(0,1)):
-                        add = add + rnd.sample(automaton.lPermitida, k = rnd.randint(0, len(automaton.lPermitida)))
+                        add = add + rnd.sample(automaton.lPermitida, k = rnd.randint(0, s))
                     cur.append(add)
-            rule.append(cur)            
+            rule.append(cur)
         automaton.lPermitida = automaton.lPermitida + list(arr) 
         return rule
     
@@ -60,15 +62,19 @@ class automaton:
     
     def __init__(self, n, rule = None):
         self.lPermitida = []
+        
         if(rule == 'A'):
             a = block(3, [0,1,2], 0, self)
             a.rule = [[ [1] ], [[2]],[[0]]]
             a.rulemat=np.array([[0,1,0],[0,0,1],[1,0,0]])
-            b = block(2, [4,5], 4, self)
+            b = block(2, [3,4], 3, self)
             b.rule = [[[4], [0,1]], [[3], [1,2]]]
             b.rulemat=np.array([[0,1],[1,0]])
             self.n = 5
             self.blocks = [a, b]
+            self.seq = [0,1,2,3,4]    
+            self.key = automaton.flatten(self)
+#            print("g_A",rule)
             return 
         elif(rule == 'B'):
             a = block(2, [3,4], 0, self)
@@ -78,25 +84,33 @@ class automaton:
             b.rule = [[[0], [3,4]], [[1], [0,3]], [[2], [0]]]
             b.rulemat = np.array([[1,0,0],[1,1,0],[1,0,1]])
             self.n = 5
-            self.blocks = [a, b] 
+            self.blocks = [a, b]
+            self.seq = [3,4,0,1,2]
+            self.key = automaton.flatten(self)
+#            print("g_B",rule)
             return
-        
+        elif(rule == 'copy'):
+            return
         self.n = n
         self.blocks = []
         self.seq = np.arange(n)
         np.random.shuffle(self.seq)
         s = 0
         while s < n:
+#            print("<")
             if(n - s < 3):
                 i = n - s
             else:
                 i = rnd.randint(3, min(5, n - s))
+#            print("i:", i)
             self.blocks.append(block(i,self.seq[s:s+i], s, self))
-            s += i
+            s += i 
+#            print(">")
         self.key = automaton.flatten(self)
+#        print()
         
     def copy(self):
-        aut = automaton(self.n, 'A')
+        aut = automaton(self.n,'copy')
         aut.blocks = self.blocks
         aut.n = self.n
         aut.seq = self.seq
@@ -166,13 +180,21 @@ class automaton:
                     #cada elemento de {x1,..., xn}
                     for l in st:
                         add = automaton.combineBlockRule(add, otherRules[l])    
-                    print("add:", add)
-                    print("replace:", replace)
-                    for x in add:
-                        if x not in replace:
-                            replace.append(x)
+                    replace = replace + add
                 self.blocks[i].rule[j] = replace
     
+    def evolve(self, state):
+        data = list(map(lambda x : int(x) - int('0'), state))
+        cypher = ""
+        for ecn in self.key:
+            cell=0
+            for i in range(len(ecn)):
+                op = 1
+                for j in ecn[i]:
+                    op = op*data[j]
+                cell = cell+op
+            cypher = cypher + str(cell%2)
+        return cypher
  
     def __str__(self):
         k = 0
@@ -215,17 +237,7 @@ class encryption:
         self.composite.key = automaton.flatten(self.composite)
             
     def encrypt(self, plain):
-        data = list(map(lambda x : int(x) - int('0'), plain))
-        ciphered = []
-        for ecn in self.key:
-            cell=0
-            for i in range(len(ecn)):
-                op = 1
-                for j in ecn[i]:
-                    op = op*data[j]
-                cell = cell+op
-            ciphered.append(cell%2)
-        return ciphered
+        return self.composite.evolve(plain)
 
     def decrypt(self, ciphered):
         for aut in reversed(self.automatons):   
@@ -241,6 +253,25 @@ class encryption:
         return str(self.composite)
             
 B = encryption(5,2)
+aut0 = B.automatons[0]
+aut1 = B.automatons[1]
 print(B.automatons[0])
 print(B.automatons[1])
 print(B)
+
+s0 = set([])
+s1 = set([])
+s2 = set([])
+for i in range(32):
+    b0 = str(bin(i))[2:]
+    if(len(b0) < 5):
+        b0 = "0"*(5-len(b0)) + b0
+    e0 = B.encrypt(b0)
+    e1 = aut0.evolve(b0)
+    e2 = aut1.evolve(b0)
+    s0.add(e0)
+    s1.add(e1)
+    s2.add(e2)
+    print("plaintext: ", b0,"\n cyphertext0: ", e0,"\n cyphertext1: ", e1,"\n cyphertext2: ", e2)    
+
+print("compuesta:", len(s0), "t=0", len(s1),"t=1", len(s2))
